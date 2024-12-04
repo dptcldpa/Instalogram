219d4be8c50d066336b33c181e04a56945c56d14
조해민 <dptcldpa@gmail.com>
2024-12-05 02:01:15 +0900

    - 로그아웃 기능 구현 진행중

--- src/main/java/com/dptcldpa/instalogram/user/UserController.java
+++ src/main/java/com/dptcldpa/instalogram/user/UserController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @RequestMapping("/user")
 @Controller
 public class UserController {
@@ -22,4 +25,14 @@ public class UserController {
 		return "user/login";
 	}
 	
+	@GetMapping("/logout")
+	public String logout(HttpServletRequest request) {
+		
+		HttpSession session = request.getSession();
+		
+		session.removeAttribute("userId");
+		session.removeAttribute("userName");
+		
+		return "redirect:/user/login-view";
+	}
 }
