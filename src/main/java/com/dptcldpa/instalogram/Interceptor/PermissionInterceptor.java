2b1dadb2a23ecb6a997e5bebbdc299c90b4da282
조해민 <dptcldpa@gmail.com>
2024-12-17 16:55:04 +0900

    - 페이지 권한 기능 구현 완료
    #2

---
+++ src/main/java/com/dptcldpa/instalogram/Interceptor/PermissionInterceptor.java
@@ -0,0 +1,43 @@
+package com.dptcldpa.instalogram.Interceptor;
+
+import java.io.IOException;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class PermissionInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(
+			HttpServletRequest request
+			, HttpServletResponse response
+			, Object handler) throws IOException {
+		
+		HttpSession session = request.getSession();
+		
+		Integer userId = (Integer)session.getAttribute("userId");
+		
+		String uri = request.getRequestURI();
+		
+		if(userId == null) {
+			if(uri.startsWith("/post")) {
+				response.sendRedirect("/user/login-view");
+				
+				return false;
+			}
+		} else {
+			if(uri.startsWith("/user")) {
+				response.sendRedirect("/post/timeline-view");
+				
+				return false;
+			}
+		}
+			
+		return true;
+		
+	}
+	
+}
--- src/main/java/com/dptcldpa/instalogram/config/WebMvcConfig.java
+++ src/main/java/com/dptcldpa/instalogram/config/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package com.dptcldpa.instalogram.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.dptcldpa.instalogram.Interceptor.PermissionInterceptor;
 import com.dptcldpa.instalogram.common.FileManager;
 
 @Configuration
@@ -17,4 +19,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		
 	}
 	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		registry.addInterceptor(new PermissionInterceptor())
+		.addPathPatterns("/**")
+		.excludePathPatterns("/user/logout", "/static/**", "/images/**");
+		
+	}
 }
